c0a3bace40e502c37fb12b1cd5e29f1937635e40
Eugene-development <52544906+Eugene-development@users.noreply.github.com>
2024-12-05 23:36:54 +0300

    c

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,21 @@
 import containerQueries from '@tailwindcss/container-queries';
 import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		extend: {},
+		fontFamily: {
+			sans: ["Mona Sans", ...defaultTheme.fontFamily.sans],
+			display: [
+			  ["Mona Sans", ...defaultTheme.fontFamily.sans],
+			  { fontVariationSettings: '"wdth" 110' },
+			],
+		  },
 	},
 
 	plugins: [typography, forms, containerQueries]
