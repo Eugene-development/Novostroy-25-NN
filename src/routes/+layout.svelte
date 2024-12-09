4bbc65d680c9b15e04f5c7022961904da7c11d97
Eugene-development <52544906+Eugene-development@users.noreply.github.com>
2024-12-09 08:51:52 +0300

    c

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,9 +3,11 @@
 	let { children } = $props();
 
 	import { Banner } from './layout/header/UI';
+	import { Menu } from './layout/header/UI';
 	import { Footer } from './layout/footer/UI';
 </script>
 
 <Banner />
+<Menu />	
 {@render children()}
 <Footer />
--- src/routes/layout/header/UI/index.js
+++ src/routes/layout/header/UI/index.js
@@ -1,1 +1,2 @@
 export { default as Banner } from './Banner/index.svelte';
+export { default as Menu } from './Menu/index.svelte';
