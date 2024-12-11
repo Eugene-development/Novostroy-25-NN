ab6b932a0c2a4ecade78d6fdb5087405f833c047
Eugene-development <52544906+Eugene-development@users.noreply.github.com>
2024-12-11 07:41:11 +0300

    с

--- src/routes/(catalog)/[slug]/+page.server.js
+++ src/routes/(catalog)/[slug]/+page.server.js
@@ -5,8 +5,11 @@ import { CATALOG } from './query';
 export async function load({ params }) {
 
 
+  const url = import.meta.env.VITE_URL;
+  // const url = process.env.VITE_URL;
+  
 
-    const url = 'https://crud.novostroy.org/graphql/'
+
     const variables = {
         key: '7eb340c2-9795-4d70-a081-aa1a0ec94a84',
         slug: params.slug
