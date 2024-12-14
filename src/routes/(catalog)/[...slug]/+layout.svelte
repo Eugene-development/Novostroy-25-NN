c30d354e61436d65300d86a1e2c68fc1fa9010d7
Eugene-development <52544906+Eugene-development@users.noreply.github.com>
2024-12-14 20:07:36 +0300

    c

---
+++ src/routes/(catalog)/[...slug]/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+    /** @type {{ data: import('./$types').LayoutData, children: import('svelte').Snippet }} */
+    let { data, children } = $props();
+</script>
+
+<p>hhh2</p>
+{@render children()}
+<p>fff2</p>
