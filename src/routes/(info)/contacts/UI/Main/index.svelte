4d8d4192b054cb36d6a1298105ddfffc4d4b9fad
Eugene-development <52544906+Eugene-development@users.noreply.github.com>
2024-12-21 18:19:39 +0300

    c

--- src/routes/(info)/contacts/UI/Main/index.svelte
+++ src/routes/(info)/contacts/UI/Main/index.svelte
@@ -32,8 +32,7 @@
 			<div>
 				<h3 class="text-base/7 font-semibold text-gray-900">Он-лайн чат</h3>
 				<p class="mt-2 text-base/7 text-gray-600">
-					Ut cursus est ut amet. Lobortis eget egestas leo vitae eget porttitor risus blandit. Nunc
-					a in lorem vel iaculis porttitor.
+					Мы всегда готовы помочь вам в режиме реального времени. Наша команда ответит на все ваши вопросы.
 				</p>
 				<div class="mt-4 text-sm/6 font-semibold">
 					<p class="text-sky-600">Чат у вас на экране <span aria-hidden="true">&rarr;</span></p>
@@ -42,27 +41,14 @@
 		</div>
 		<div class="flex gap-x-6">
 			<div class="flex size-10 shrink-0 items-center justify-center rounded-lg bg-sky-600">
-				<svg
-					class="size-6 text-white"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke-width="1.5"
-					stroke="currentColor"
-					aria-hidden="true"
-					data-slot="icon"
-				>
-					<path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						d="M12 12.75c1.148 0 2.278.08 3.383.237 1.037.146 1.866.966 1.866 2.013 0 3.728-2.35 6.75-5.25 6.75S6.75 18.728 6.75 15c0-1.046.83-1.867 1.866-2.013A24.204 24.204 0 0 1 12 12.75Zm0 0c2.883 0 5.647.508 8.207 1.44a23.91 23.91 0 0 1-1.152 6.06M12 12.75c-2.883 0-5.647.508-8.208 1.44.125 2.104.52 4.136 1.153 6.06M12 12.75a2.25 2.25 0 0 0 2.248-2.354M12 12.75a2.25 2.25 0 0 1-2.248-2.354M12 8.25c.995 0 1.971-.08 2.922-.236.403-.066.74-.358.795-.762a3.778 3.778 0 0 0-.399-2.25M12 8.25c-.995 0-1.97-.08-2.922-.236-.402-.066-.74-.358-.795-.762a3.734 3.734 0 0 1 .4-2.253M12 8.25a2.25 2.25 0 0 0-2.248 2.146M12 8.25a2.25 2.25 0 0 1 2.248 2.146M8.683 5a6.032 6.032 0 0 1-1.155-1.002c.07-.63.27-1.222.574-1.747m.581 2.749A3.75 3.75 0 0 1 15.318 5m0 0c.427-.283.815-.62 1.155-.999a4.471 4.471 0 0 0-.575-1.752M4.921 6a24.048 24.048 0 0 0-.392 3.314c1.668.546 3.416.914 5.223 1.082M19.08 6c.205 1.08.337 2.187.392 3.314a23.882 23.882 0 0 1-5.223 1.082"
-					/>
-				</svg>
+				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 text-white">
+					<path stroke-linecap="round" stroke-linejoin="round" d="M14.25 9.75v-4.5m0 4.5h4.5m-4.5 0 6-6m-3 18c-8.284 0-15-6.716-15-15V4.5A2.25 2.25 0 0 1 4.5 2.25h1.372c.516 0 .966.351 1.091.852l1.106 4.423c.11.44-.054.902-.417 1.173l-1.293.97a1.062 1.062 0 0 0-.38 1.21 12.035 12.035 0 0 0 7.143 7.143c.441.162.928-.004 1.21-.38l.97-1.293a1.125 1.125 0 0 1 1.173-.417l4.423 1.106c.5.125.852.575.852 1.091V19.5a2.25 2.25 0 0 1-2.25 2.25h-2.25Z" />
+				  </svg>
 			</div>
 			<div>
 				<h3 class="text-base/7 font-semibold text-gray-900">Телефон</h3>
 				<p class="mt-2 text-base/7 text-gray-600">
-					Expedita qui non ut quia ipsum voluptatum ipsam pariatur. Culpa vitae ipsum minus eius
-					vero quo quibusdam.
+					Позвоните нам, и мы проконсультируем вас по любым вопросам. Работаем с понедельника по субботу.
 				</p>
 				<div class="mt-4 text-sm/6 font-semibold">
 					<p class="text-sky-600">8 (915) 400-00-20</p>
@@ -71,27 +57,16 @@
 		</div>
 		<div class="flex gap-x-6">
 			<div class="flex size-10 shrink-0 items-center justify-center rounded-lg bg-sky-600">
-				<svg
-					class="size-6 text-white"
-					fill="none"
-					viewBox="0 0 24 24"
-					stroke-width="1.5"
-					stroke="currentColor"
-					aria-hidden="true"
-					data-slot="icon"
-				>
-					<path
-						stroke-linecap="round"
-						stroke-linejoin="round"
-						d="M9 17.25v1.007a3 3 0 0 1-.879 2.122L7.5 21h9l-.621-.621A3 3 0 0 1 15 18.257V17.25m6-12V15a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 15V5.25m18 0A2.25 2.25 0 0 0 18.75 3H5.25A2.25 2.25 0 0 0 3 5.25m18 0V12a2.25 2.25 0 0 1-2.25 2.25H5.25A2.25 2.25 0 0 1 3 12V5.25"
-					/>
-				</svg>
+				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 text-white">
+					<path stroke-linecap="round" stroke-linejoin="round" d="M16.5 12a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0Zm0 0c0 1.657 1.007 3 2.25 3S21 13.657 21 12a9 9 0 1 0-2.636 6.364M16.5 12V8.25" />
+				  </svg>
+				  
+				  
 			</div>
 			<div>
 				<h3 class="text-base/7 font-semibold text-gray-900">Почта</h3>
 				<p class="mt-2 text-base/7 text-gray-600">
-					Sint aut modi porro consequatur architecto commodi qui consequatur. Dignissimos adipisci
-					minima.
+					Свяжитесь с нами по электронной почте. Мы постараемся ответить в течение 24 часов.
 				</p>
 				<div class="mt-4 text-sm/6 font-semibold">
 					<p class="text-sky-600">info@novostroy.org</p>
--- src/routes/layout/header/UI/Menu/index.svelte
+++ src/routes/layout/header/UI/Menu/index.svelte
@@ -41,17 +41,6 @@
 				</button>
 
 				{#if visibleServicesMenu}
-
-				<!--
-    Flyout menu, show/hide based on flyout menu state.
-
-    Entering: "transition ease-out duration-200"
-      From: "opacity-0 translate-y-1"
-      To: "opacity-100 translate-y-0"
-    Leaving: "transition ease-in duration-150"
-      From: "opacity-100 translate-y-0"
-      To: "opacity-0 translate-y-1"
-  -->
 					<div transition:fade={{ duration: 50, delay: 50 }}
 						role="menu"
 						tabindex="0"
