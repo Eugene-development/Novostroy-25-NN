4f9a4840822694c2c41913ee45fbfd2c5d330e43
Eugene-development <52544906+Eugene-development@users.noreply.github.com>
2024-12-30 19:31:09 +0300

    с

--- src/routes/(info)/vacancy/marketer/UI/Main/index.svelte
+++ src/routes/(info)/vacancy/marketer/UI/Main/index.svelte
@@ -1,1 +1,41 @@
-<p>Маркетолог</p>
+<div class="bg-white py-24 sm:py-32">
+    <div class="mx-auto max-w-7xl px-6 lg:px-8">
+      <div class="mx-auto  max-w-7xl  items-start  lg:mx-0 lg:max-w-none ">
+        
+        <div>
+          <div class="text-base/7 text-gray-700 lg:max-w-5xl">
+            <p class="text-base/7 font-semibold text-sky-600">Вакансия</p>
+            <h1 class="mt-2 text-pretty text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">Маркетолог</h1>
+            <div class="max-w-xl">
+              <p class="mt-6">Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra tellus varius sit neque erat velit. Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae sed turpis id.</p>
+            </div>
+            <div class="max-w-xl">
+              <p class="mt-6">Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra tellus varius sit neque erat velit. Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae sed turpis id.</p>
+            </div>
+          </div>
+          <dl class="mt-10 grid grid-cols-2 gap-8 border-t border-gray-900/10 pt-10 sm:grid-cols-4">
+            <div>
+              <dt class="text-sm/6 font-semibold text-gray-600">Трудоустройство</dt>
+              <dd class="mt-2 text-3xl/10 font-bold tracking-tight text-gray-900">Официально</dd>
+            </div>
+            <div>
+              <dt class="text-sm/6 font-semibold text-gray-600">Отпуск</dt>
+              <dd class="mt-2 text-3xl/10 font-bold tracking-tight text-gray-900">Имеется</dd>
+            </div>
+            <div>
+              <dt class="text-sm/6 font-semibold text-gray-600">Зарплата</dt>
+              <dd class="mt-2 text-3xl/10 font-bold tracking-tight text-gray-900">Достойная</dd>
+            </div>
+            <div>
+              <dt class="text-sm/6 font-semibold text-gray-600">Компания</dt>
+              <dd class="mt-2 text-3xl/10 font-bold tracking-tight text-gray-900">Надёжная</dd>
+            </div>
+          </dl>
+          <!-- <div class="mt-10 flex">
+            <a href="/" class="text-base/7 font-semibold text-indigo-600">Learn more about our company <span aria-hidden="true">&rarr;</span></a>
+          </div> -->
+        </div>
+      </div>
+    </div>
+  </div>
+  
