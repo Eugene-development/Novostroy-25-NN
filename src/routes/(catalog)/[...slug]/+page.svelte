a999f6f989232fd06ee9b3bc5c8e7233d6550526
Eugene-development <52544906+Eugene-development@users.noreply.github.com>
2025-01-02 16:30:50 +0300

    c

--- src/routes/(catalog)/[...slug]/+page.svelte
+++ src/routes/(catalog)/[...slug]/+page.svelte
@@ -1,12 +1,19 @@
 <script>
 	/** @type {{ data: import('./$types').PageData }} */
 
-	import { Rubric, Category, Product } from '../UI';
+	import { Catalog, Category, Product } from '../UI';
 
 	let { data } = $props();
 	$inspect(data);
 </script>
 
 <main class="h-full flex-1 overflow-y-auto py-3 lg:pl-4">
-	<Rubric data={data.req.catalog} />
+	
+	{#if data.isCatalog}
+	<Catalog data={data.req.catalog} />
+	{:else if data.isCategory}
+	<Category data={data.req.category} />
+	{:else if data.isProduct}
+	<Product data={data.req.product} />
+	{/if}
 </main>
