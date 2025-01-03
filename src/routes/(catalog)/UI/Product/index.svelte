9728f299ff625c06de968c09acdf44e0ee05c02e
Eugene-development <52544906+Eugene-development@users.noreply.github.com>
2025-01-03 20:45:07 +0300

    c

--- src/routes/(catalog)/UI/Product/index.svelte
+++ src/routes/(catalog)/UI/Product/index.svelte
@@ -53,7 +53,6 @@
             <h3 class="sr-only">Reviews</h3>
             <div class="flex items-center">
               <div class="flex items-center">
-                <!-- Active: "text-indigo-500", Inactive: "text-gray-300" -->
                 <svg class="size-5 shrink-0 text-red-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
                   <path fill-rule="evenodd" d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401Z" clip-rule="evenodd" />
                 </svg>
@@ -70,7 +69,7 @@
                   <path fill-rule="evenodd" d="M10.868 2.884c-.321-.772-1.415-.772-1.736 0l-1.83 4.401-4.753.381c-.833.067-1.171 1.107-.536 1.651l3.62 3.102-1.106 4.637c-.194.813.691 1.456 1.405 1.02L10 15.591l4.069 2.485c.713.436 1.598-.207 1.404-1.02l-1.106-4.637 3.62-3.102c.635-.544.297-1.584-.536-1.65l-4.752-.382-1.831-4.401Z" clip-rule="evenodd" />
                 </svg>
               </div>
-              <p class="sr-only">4 out of 5 stars</p>
+              <p class="sr-only">5 out of 5 stars</p>
             </div>
           </div>
   
