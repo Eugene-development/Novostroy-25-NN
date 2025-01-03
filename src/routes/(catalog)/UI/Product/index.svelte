b2b160e1004be767329ba2a928d2e989c92a55de
Eugene-development <52544906+Eugene-development@users.noreply.github.com>
2025-01-03 22:45:39 +0300

    c

--- src/routes/(catalog)/UI/Product/index.svelte
+++ src/routes/(catalog)/UI/Product/index.svelte
@@ -3,7 +3,7 @@
 
 	let { data } = $props();
 
-	// $inspect(data);
+	$inspect(data);
 </script>
 
 
@@ -18,7 +18,7 @@
               <button id="tabs-1-tab-1" class="relative flex h-24 cursor-pointer items-center justify-center rounded-md bg-white text-sm font-medium uppercase text-gray-900 hover:bg-gray-50 focus:outline-none focus:ring focus:ring-indigo-500/50 focus:ring-offset-4" aria-controls="tabs-1-panel-1" role="tab" type="button">
                 <span class="sr-only">Angled view</span>
                 <span class="absolute inset-0 overflow-hidden rounded-md">
-                  <img src={`${import.meta.env.VITE_S3}/catalog/${data.image[0].hash}`} alt="" class="size-full object-cover">
+                  <img src={`${import.meta.env.VITE_S3}/catalog/${data.image[0].hash}`} alt="" class="size-full object-contain">
                 </span>
                 <!-- Selected: "ring-indigo-500", Not Selected: "ring-transparent" -->
                 <span class="pointer-events-none absolute inset-0 rounded-md ring-2 ring-transparent ring-offset-2" aria-hidden="true"></span>
@@ -28,10 +28,10 @@
             </div>
           </div>
   
-          <div>
+          <div >
             <!-- Tab panel, show/hide based on tab state. -->
-            <div id="tabs-1-panel-1" aria-labelledby="tabs-1-tab-1" role="tabpanel" tabindex="0">
-              <img src={`${import.meta.env.VITE_S3}/catalog/${data.image[0].hash}`} alt="Angled front view with bag zipped and handles upright." class="aspect-square w-full object-cover sm:rounded-lg">
+            <div id="tabs-1-panel-1" aria-labelledby="tabs-1-tab-1" role="tabpanel" tabindex="0" class="cursor-pointer w-full aspect-w-15 aspect-h-10 scale110">
+              <img src={`${import.meta.env.VITE_S3}/catalog/${data.image[0].hash}`} alt="Angled front view with bag zipped and handles upright." class=" w-full object-contain sm:rounded-lg">
             </div>
   
             <!-- More images... -->
@@ -39,12 +39,12 @@
         </div>
   
         <!-- Product info -->
-        <div class="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
+        <div class="mt-10 px-4 sm:mt-16  lg:mt-0">
           <h1 class="text-3xl font-bold tracking-tight text-gray-900">{data.value}</h1>
   
           <div class="mt-3">
             <h2 class="sr-only">{data.parentable.value}</h2>
-            <p class="text-xl tracking-tight text-gray-900">{data.parentable.value}</p>
+            <a href="/{data.parentable.parentable.parentable.slug}/{data.parentable.parentable.slug}/{data.parentable.slug}" class="text-xl tracking-tight text-gray-900">{data.parentable.value}</a>
           </div>
   
           <!-- Reviews -->
@@ -76,21 +76,22 @@
             <h3 class="sr-only">Description</h3>
   
             <div class="space-y-6 text-base text-gray-700">
-              <p>The Zip Tote Basket is the perfect midpoint between shopping tote and comfy backpack. With convertible straps, you can hand carry, should sling, or backpack this convenient and spacious bag. The zip top and durable canvas construction keeps your goods protected for all-day use.</p>
-            </div>
+                <p>Наш дизайнер бесплатно даст вам общую консультацию по интересующим вас вопросам, связанным с проектированием мебели и условиям нашей работы.</p>
+            	<p>Мы дадим советы по выбору материалов и цветов и составим для вас стартовый дизайн-проект.</p>
+			</div>
           </div>
   
           <form class="mt-6">
             
   
             <div class="mt-10 flex">
-              <button type="submit" class="flex max-w-xs flex-1 items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50 sm:w-full">Add to bag</button>
+              <button type="submit" class="flex max-w-xs flex-1 items-center justify-center rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50 sm:w-full">Добавьте в избранное</button>
   
               <button type="button" class="ml-4 flex items-center justify-center rounded-md px-3 py-3 text-gray-400 hover:bg-gray-100 hover:text-gray-500">
                 <svg class="size-6 shrink-0" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
                   <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
                 </svg>
-                <span class="sr-only">Add to favorites</span>
+                <span class="sr-only">Добавьте в избранное</span>
               </button>
             </div>
           </form>
@@ -101,36 +102,18 @@
             <div class="divide-y divide-gray-200 border-t">
               <div>
                 <h3>
-                  <!-- Expand/collapse question button -->
-                  <button type="button" class="group relative flex w-full items-center justify-between py-6 text-left" aria-controls="disclosure-1" aria-expanded="false">
-                    <!-- Open: "text-indigo-600", Closed: "text-gray-900" -->
-                    <span class="text-sm font-medium text-gray-900">Features</span>
-                    <span class="ml-6 flex items-center">
-                      <!-- Open: "hidden", Closed: "block" -->
-                      <svg class="block size-6 text-gray-400 group-hover:text-gray-500" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
-                      </svg>
-                      <!-- Open: "block", Closed: "hidden" -->
-                      <svg class="hidden size-6 text-indigo-400 group-hover:text-indigo-500" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M5 12h14" />
-                      </svg>
-                    </span>
-                  </button>
+                  <div class="group relative flex w-full items-center justify-between py-6 text-left" >
+                    <span class="text-sm font-medium text-gray-900">Теги</span>             
+				  </div>
                 </h3>
                 <div class="pb-6" id="disclosure-1">
                   <ul role="list" class="list-disc space-y-1 pl-5 text-sm/6 text-gray-700 marker:text-gray-300">
-                    <li class="pl-2">Multiple strap configurations</li>
-                    <li class="pl-2">Spacious interior with top zip</li>
-                    <li class="pl-2">Leather handle and tabs</li>
-                    <li class="pl-2">Interior dividers</li>
-                    <li class="pl-2">Stainless strap loops</li>
-                    <li class="pl-2">Double stitched construction</li>
-                    <li class="pl-2">Water-resistant</li>
+					{#each data.tag as item}
+						<li class="pl-2">{item.value}</li>
+					{/each}
                   </ul>
               </div>
-  
-              <!-- More sections... -->
-            </div>
+              </div>
           </section>
         </div>
       </div>
