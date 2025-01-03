8b0eae2ff99d559b9bcc0ba17669a65b54449796
Eugene-development <52544906+Eugene-development@users.noreply.github.com>
2025-01-03 16:52:55 +0300

    с

--- src/routes/(catalog)/UI/Category/SwiperSlider/index.svelte
+++ src/routes/(catalog)/UI/Category/SwiperSlider/index.svelte
@@ -1,39 +1,17 @@
 <script>
 import { Splide, SplideSlide } from '@splidejs/svelte-splide';
-
-// Default theme
 import '@splidejs/svelte-splide/css';
-// or other themes
-import '@splidejs/svelte-splide/css/skyblue';
-import '@splidejs/svelte-splide/css/sea-green';
-// or only core styles
-import '@splidejs/svelte-splide/css/core';
 
 
 let { data } = $props();
-
 </script>
-<section class="py-2 w-72 sm:w-full mx-auto">
-    <div class="container">
-<Splide aria-label="My Favorite Images" 
-options={ {
-    rewind: true,
-    width : 800,
-    gap   : '1rem',
-  } }>
 
+<Splide aria-label="Проекты">
     {#each data as item}
-    
-        <SplideSlide >
-            <div class="w-full aspect-w-15 aspect-h-12">
-            <img src={`${import.meta.env.VITE_S3}/catalog/${item.hash}`} alt="Slide showing a placeholder for Slide 1" class="w-full object-cover object-center"/>
-        </div>
-        </SplideSlide>
-   
+      <SplideSlide>
+        <img src={`${import.meta.env.VITE_S3}/catalog/${item.hash}`} alt="Выполненный проект" class="w-full h-80 object-contain"/>
+      </SplideSlide>
     {/each}
-   
-    
-</Splide>
+  </Splide>
 
-    </div>
-</section>
+
