9ad2da6fe3cbb19c017e4972f73bfaea717c82ab
Eugene-development <52544906+Eugene-development@users.noreply.github.com>
2025-01-04 18:58:04 +0300

    с

--- src/routes/(content)/blog/UI/Main/index.svelte
+++ src/routes/(content)/blog/UI/Main/index.svelte
@@ -2,10 +2,10 @@
 	<div class="mx-auto max-w-7xl px-6 lg:px-8">
 		<div class="mx-auto max-w-2xl lg:mx-0">
 			<h2 class="text-pretty text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
-				From the blog
+				Наш Блог
 			</h2>
 			<p class="mt-2 text-lg/8 text-gray-600">
-				Learn how to grow your business with our expert advice.
+				Получайте полезные советы и новости от наших специалистов.
 			</p>
 		</div>
 		<div
