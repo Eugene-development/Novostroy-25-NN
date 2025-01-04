264dd8dd1daa16bec91ba804bf2adb70902c729f
Eugene-development <52544906+Eugene-development@users.noreply.github.com>
2025-01-04 19:43:01 +0300

    c

--- src/routes/(info)/actions/UI/Main/index.svelte
+++ src/routes/(info)/actions/UI/Main/index.svelte
@@ -2,187 +2,46 @@
 	import MCardBody1 from './MCardBody/index.svelte';
 </script>
 
-<section class="bg-white animate-in fade-in-10 duration-300">
+<section class="my-12 bg-white animate-in fade-in-10 duration-300">
 	<div class="mx-auto max-w-screen-xl px-4 py-8 sm:py-16 lg:px-6">
-		<h2 class="mb-4 text-4xl font-extrabold tracking-tight text-gray-900">
-			We didn't reinvent the wheel
-		</h2>
-		<p class="mb-3 font-light text-gray-500 sm:text-xl">
-			Track work across the enterprise through an open, collaborative platform. Link issues across
-			Jira and ingest data from other software development tools, so your IT support and operations
-			teams have richer contextual information to rapidly respond to requests, incidents, and
-			changes.
-		</p>
-		<p class="mb-8 font-light text-gray-500 sm:text-xl">
-			Deliver great service experiences fast - without the complexity of traditional ITSM
-			solutions.Accelerate critical development work, eliminate toil, and deploy changes with ease.
-		</p>
+		<div class="mx-auto mb-8 max-w-screen-sm text-center">
+			<h2 class="mb-4 text-4xl font-extrabold tracking-tight text-gray-900">
+				Акции наших партнеров
+			</h2>
+			<p class="mb-3 font-light text-gray-500 sm:text-xl">
+				Получите актуальные акции наших партнеров и узнайте о скидках и скидках на товары
+			</p>
+		</div>
 		<div class="grid gap-8 lg:grid-cols-2">
-			<div class="w-full py-6">
+			<div class="w-full py-10">
 				<div class="relative mx-auto h-72 max-w-xl sm:h-52">
 					<div
-						class=" absolute -top-4 size-full scale-x-90 scale-y-[1.15] rounded-3xl border border-neutral-200 bg-zinc-100"
+						class=" absolute -top-4 size-full scale-x-90 scale-y-[1.15] rounded-xl border border-gray-200 bg-gray-50"
 						style="transform-origin: top center"
 					></div>
 					<div
-						class="absolute flex size-full items-center justify-center rounded-3xl border border-neutral-200 bg-white p-2 shadow-[0px_0px_16px_#D4D4D8] md:p-6 dark:shadow-[0px_0px_64px_rgba(39,39,42,0.6)]"
+						class="absolute flex size-full items-center justify-center rounded-2xl border border-gray-200 bg-white p-2 shadow-[0px_0px_48px_rgba(39,39,42,0.6)] md:p-6 "
 						style="transform-origin: top center"
 					>
-						<slot>
-							<MCardBody1 />
-						</slot>
-					</div>
-				</div>
-			</div>
-			<div class="w-full py-6">
-				<div class="relative mx-auto h-72 max-w-xl sm:h-52">
-					<div
-						class=" absolute -top-4 size-full scale-x-90 scale-y-[1.15] rounded-3xl border border-neutral-200 bg-zinc-100"
-						style="transform-origin: top center"
-					></div>
-					<div
-						class="absolute flex size-full items-center justify-center rounded-3xl border border-neutral-200 bg-white p-2 shadow-[0px_0px_16px_#D4D4D8] md:p-6 dark:shadow-[0px_0px_64px_rgba(39,39,42,0.6)]"
-						style="transform-origin: top center"
-					>
-						<slot>
-							<MCardBody1 />
-						</slot>
-					</div>
-				</div>
-			</div>
-			<div class="w-full py-6">
-				<div class="relative mx-auto h-72 max-w-xl sm:h-52">
-					<div
-						class=" absolute -top-4 size-full scale-x-90 scale-y-[1.15] rounded-3xl border border-neutral-200 bg-zinc-100"
-						style="transform-origin: top center"
-					></div>
-					<div
-						class="absolute flex size-full items-center justify-center rounded-3xl border border-neutral-200 bg-white p-2 shadow-[0px_0px_16px_#D4D4D8] md:p-6 dark:shadow-[0px_0px_64px_rgba(39,39,42,0.6)]"
-						style="transform-origin: top center"
-					>
-						<slot>
-							<MCardBody1 />
-						</slot>
-					</div>
-				</div>
-			</div>
-			<div class="w-full py-6">
-				<div class="relative mx-auto h-72 max-w-xl sm:h-52">
-					<div
-						class=" absolute -top-4 size-full scale-x-90 scale-y-[1.15] rounded-3xl border border-neutral-200 bg-zinc-100"
-						style="transform-origin: top center"
-					></div>
-					<div
-						class="absolute flex size-full items-center justify-center rounded-3xl border border-neutral-200 bg-white p-2 shadow-[0px_0px_16px_#D4D4D8] md:p-6 dark:shadow-[0px_0px_64px_rgba(39,39,42,0.6)]"
-						style="transform-origin: top center"
-					>
-						<slot>
-							<MCardBody1 />
-						</slot>
-					</div>
-				</div>
-			</div>
-			<div class="w-full py-6">
-				<div class="relative mx-auto h-72 max-w-xl sm:h-52">
-					<div
-						class=" absolute -top-4 size-full scale-x-90 scale-y-[1.15] rounded-3xl border border-neutral-200 bg-zinc-100"
-						style="transform-origin: top center"
-					></div>
-					<div
-						class="absolute flex size-full items-center justify-center rounded-3xl border border-neutral-200 bg-white p-2 shadow-[0px_0px_16px_#D4D4D8] md:p-6 dark:shadow-[0px_0px_64px_rgba(39,39,42,0.6)]"
-						style="transform-origin: top center"
-					>
-						<slot>
-							<MCardBody1 />
-						</slot>
-					</div>
-				</div>
-			</div>
-			<div class="w-full py-6">
-				<div class="relative mx-auto h-72 max-w-xl sm:h-52">
-					<div
-						class=" absolute -top-4 size-full scale-x-90 scale-y-[1.15] rounded-3xl border border-neutral-200 bg-zinc-100"
-						style="transform-origin: top center"
-					></div>
-					<div
-						class="absolute flex size-full items-center justify-center rounded-3xl border border-neutral-200 bg-white p-2 shadow-[0px_0px_16px_#D4D4D8] md:p-6 dark:shadow-[0px_0px_64px_rgba(39,39,42,0.6)]"
-						style="transform-origin: top center"
-					>
-						<slot>
-							<MCardBody1 />
-						</slot>
-					</div>
-				</div>
-			</div>
+					<div>
+						<div class="flex mb-1 items-center gap-2">
+							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 fill-red-500 text-pink-500">
+								<path stroke-linecap="round" stroke-linejoin="round" d="m3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z" />
+							  </svg>
+							<h3 class=" text-2xl font-bold text-gray-900">
+								Бесплатная консультация
+							</h3>
+						</div>
+						<p class="mt-4 text-gray-700">
+							Наши дизайнеры дадут вам бесплатную консультацию по выбору технологии и
+							модели.
+						</p>
 
-			<a
-				href="/"
-				class="inline-flex items-center justify-center rounded-lg bg-gray-50 p-5 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-900"
-			>
-				<span class="w-full">Бесплатная консультация.</span>
-				<svg
-					class="ml-3 h-6 w-6"
-					fill="currentColor"
-					viewBox="0 0 20 20"
-					xmlns="http://www.w3.org/2000/svg"
-					><path
-						fill-rule="evenodd"
-						d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
-						clip-rule="evenodd"
-					></path></svg
-				>
-			</a>
-			<a
-				href="/"
-				class="inline-flex items-center justify-center rounded-lg bg-gray-50 p-5 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-900"
-			>
-				<span class="w-full">Explore Flowbite and take your marketplace to the next level.</span>
-				<svg
-					class="ml-3 h-6 w-6"
-					fill="currentColor"
-					viewBox="0 0 20 20"
-					xmlns="http://www.w3.org/2000/svg"
-					><path
-						fill-rule="evenodd"
-						d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
-						clip-rule="evenodd"
-					></path></svg
-				>
-			</a>
-			<a
-				href="/"
-				class="inline-flex items-center justify-center rounded-lg bg-gray-50 p-5 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-900"
-			>
-				<span class="w-full">Explore Flowbite and take your marketplace to the next level.</span>
-				<svg
-					class="ml-3 h-6 w-6"
-					fill="currentColor"
-					viewBox="0 0 20 20"
-					xmlns="http://www.w3.org/2000/svg"
-					><path
-						fill-rule="evenodd"
-						d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
-						clip-rule="evenodd"
-					></path></svg
-				>
-			</a>
-			<a
-				href="/"
-				class="inline-flex items-center justify-center rounded-lg bg-gray-50 p-5 text-base font-medium text-gray-500 hover:bg-gray-100 hover:text-gray-900"
-			>
-				<span class="w-full">Explore Flowbite and take your marketplace to the next level.</span>
-				<svg
-					class="ml-3 h-6 w-6"
-					fill="currentColor"
-					viewBox="0 0 20 20"
-					xmlns="http://www.w3.org/2000/svg"
-					><path
-						fill-rule="evenodd"
-						d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
-						clip-rule="evenodd"
-					></path></svg
-				>
-			</a>
+					</div>
+					
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 </section>
