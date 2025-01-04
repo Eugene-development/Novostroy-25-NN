1bd4b32515cba101ad587003abb29e5b36fe6233
Eugene-development <52544906+Eugene-development@users.noreply.github.com>
2025-01-04 15:47:38 +0300

    с

--- src/routes/(info)/installment/UI/Main/index.svelte
+++ src/routes/(info)/installment/UI/Main/index.svelte
@@ -57,16 +57,15 @@
 					<svg viewBox="0 0 4 4" class="mr-4 size-1 flex-none" aria-hidden="true">
 						<circle cx="2" cy="2" r="2" fill="currentColor" />
 					</svg>
-					Aug 2021
+					Рассрочка
 					<div
 						class="absolute -ml-2 h-px w-screen -translate-x-full bg-gray-900/10 sm:-ml-4 lg:static lg:-mr-6 lg:ml-8 lg:w-auto lg:flex-auto lg:translate-x-0"
 						aria-hidden="true"
 					></div>
 				</time>
-				<p class="mt-6 text-lg/8 font-semibold tracking-tight text-gray-900">Founded company</p>
+				<p class="mt-6 text-lg/8 font-semibold tracking-tight text-gray-900">20-0-6</p>
 				<p class="mt-1 text-base/7 text-gray-600">
-					Nihil aut nam. Dignissimos a pariatur et quos omnis. Aspernatur asperiores et dolorem
-					dolorem optio voluptate repudiandae.
+					Выгодная рассрочка на шесть месяцев с первоначальным взносом от 0 до 20%.
 				</p>
 			</div>
 			<div>
@@ -74,18 +73,15 @@
 					<svg viewBox="0 0 4 4" class="mr-4 size-1 flex-none" aria-hidden="true">
 						<circle cx="2" cy="2" r="2" fill="currentColor" />
 					</svg>
-					Dec 2021
+					Рассрочка
 					<div
 						class="absolute -ml-2 h-px w-screen -translate-x-full bg-gray-900/10 sm:-ml-4 lg:static lg:-mr-6 lg:ml-8 lg:w-auto lg:flex-auto lg:translate-x-0"
 						aria-hidden="true"
 					></div>
 				</time>
-				<p class="mt-6 text-lg/8 font-semibold tracking-tight text-gray-900">
-					Secured $65m in funding
-				</p>
+				<p class="mt-6 text-lg/8 font-semibold tracking-tight text-gray-900">20-0-12</p>
 				<p class="mt-1 text-base/7 text-gray-600">
-					Provident quia ut esse. Vero vel eos repudiandae aspernatur. Cumque minima impedit
-					sapiente a architecto nihil.
+					Популярная рассрочка на десять месяцев с первоначальным взносом от 0 до 20%.
 				</p>
 			</div>
 			<div>
@@ -93,16 +89,15 @@
 					<svg viewBox="0 0 4 4" class="mr-4 size-1 flex-none" aria-hidden="true">
 						<circle cx="2" cy="2" r="2" fill="currentColor" />
 					</svg>
-					Feb 2022
+					Рассрочка
 					<div
 						class="absolute -ml-2 h-px w-screen -translate-x-full bg-gray-900/10 sm:-ml-4 lg:static lg:-mr-6 lg:ml-8 lg:w-auto lg:flex-auto lg:translate-x-0"
 						aria-hidden="true"
 					></div>
 				</time>
-				<p class="mt-6 text-lg/8 font-semibold tracking-tight text-gray-900">Released beta</p>
+				<p class="mt-6 text-lg/8 font-semibold tracking-tight text-gray-900">20-0-12</p>
 				<p class="mt-1 text-base/7 text-gray-600">
-					Sunt perspiciatis incidunt. Non necessitatibus aliquid. Consequatur ut officiis earum eum
-					quia facilis. Hic deleniti dolorem quia et.
+					Рассрочка на двенадцать месяцев с первоначальным взносом от 0 до 20%.
 				</p>
 			</div>
 			<div>
@@ -110,18 +105,16 @@
 					<svg viewBox="0 0 4 4" class="mr-4 size-1 flex-none" aria-hidden="true">
 						<circle cx="2" cy="2" r="2" fill="currentColor" />
 					</svg>
-					Dec 2022
+					Кредит
 					<div
 						class="absolute -ml-2 h-px w-screen -translate-x-full bg-gray-900/10 sm:-ml-4 lg:static lg:-mr-6 lg:ml-8 lg:w-auto lg:flex-auto lg:translate-x-0"
 						aria-hidden="true"
 					></div>
 				</time>
-				<p class="mt-6 text-lg/8 font-semibold tracking-tight text-gray-900">
-					Global launch of product
-				</p>
+				<p class="mt-6 text-lg/8 font-semibold tracking-tight text-gray-900">20-0-24</p>
 				<p class="mt-1 text-base/7 text-gray-600">
-					Ut ipsa sint distinctio quod itaque nam qui. Possimus aut unde id architecto voluptatem
-					hic aut pariatur velit.
+					Кредит на один год с первым взносом от 0 до 20%. Проценты по кредиту
+					включены в цену.
 				</p>
 			</div>
 		</div>
