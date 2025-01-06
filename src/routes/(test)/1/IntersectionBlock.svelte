c29f14520d2974b804e9056320ffab6758b63823
Eugene-development <52544906+Eugene-development@users.noreply.github.com>
2025-01-06 14:18:52 +0300

    с

---
+++ src/routes/(test)/1/IntersectionBlock.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	export let isIntersecting: boolean;
+</script>
+
+{#if isIntersecting}
+	<div class="bg-green-500 p-4 text-white">Target is intersecting</div>
+{:else}
+	<div class="bg-red-500 p-4 text-white">Target is not intersecting</div>
+{/if}
