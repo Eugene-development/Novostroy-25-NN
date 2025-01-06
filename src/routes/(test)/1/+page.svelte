6c2e3a3c4705aa1bf77200adecf9136e962d472c
Eugene-development <52544906+Eugene-development@users.noreply.github.com>
2025-01-06 14:26:28 +0300

    c

--- src/routes/(test)/1/+page.svelte
+++ src/routes/(test)/1/+page.svelte
@@ -28,6 +28,6 @@
 	<DeploySection />
 	
 	<div bind:this={target} >
-		<IntersectionBlock {isIntersecting} />
+		<IntersectionBlock data={isIntersecting} />
 	</div>
 </div>
--- src/routes/(test)/1/IntersectionBlock.svelte
+++ src/routes/(test)/1/IntersectionBlock.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-	export let isIntersecting: boolean;
+	let { data } = $props();
 </script>
 
-{#if isIntersecting}
+{#if data.isIntersecting}
 	<div class="bg-green-500 p-4 text-white">Target is intersecting</div>
 {:else}
 	<div class="bg-red-500 p-4 text-white">Target is not intersecting</div>
