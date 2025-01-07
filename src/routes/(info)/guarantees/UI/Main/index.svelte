056dab9c5402f264f1bd9c4bcae39cb903e95bee
Eugene-development <52544906+Eugene-development@users.noreply.github.com>
2025-01-07 18:41:28 +0300

    с

--- src/routes/(info)/guarantees/UI/Main/index.svelte
+++ src/routes/(info)/guarantees/UI/Main/index.svelte
@@ -3,10 +3,10 @@
 		<div class="mx-auto max-w-2xl lg:max-w-none">
 			<div class="text-center">
 				<h2 class="text-balance text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
-					Trusted by creators worldwide
+					Расширенная гарантия
 				</h2>
 				<p class="mt-4 text-lg/8 text-gray-600">
-					Lorem ipsum dolor sit amet consect adipisicing possimus.
+					Вся продукция имеет расширенную гарантию на материалы и работу мастеров.
 				</p>
 			</div>
 			<dl
@@ -41,31 +41,11 @@
 					<dd class="order-first text-3xl font-semibold tracking-tight text-gray-900">24-48 месяцев</dd>
 				</div>
 				<div class="flex flex-col bg-gray-400/5 p-8">
-					<dt class="text-sm/6 font-semibold text-gray-600">Uptime guarantee</dt>
+					<dt class="text-sm/6 font-semibold text-gray-600">На мойки</dt>
 					<dd class="order-first text-3xl font-semibold tracking-tight text-gray-900">99.9%</dd>
 				</div>
 				<div class="flex flex-col bg-gray-400/5 p-8">
-					<dt class="text-sm/6 font-semibold text-gray-600">Paid out to creators</dt>
-					<dd class="order-first text-3xl font-semibold tracking-tight text-gray-900">$70M</dd>
-				</div>
-			</dl>
-			<dl
-				class="mt-16 grid grid-cols-1 gap-0.5 overflow-hidden rounded-2xl text-center sm:grid-cols-2 lg:grid-cols-4"
-			>
-				<div class="flex flex-col bg-gray-400/5 p-8">
-					<dt class="text-sm/6 font-semibold text-gray-600">Creators on the platform</dt>
-					<dd class="order-first text-3xl font-semibold tracking-tight text-gray-900">8,000+</dd>
-				</div>
-				<div class="flex flex-col bg-gray-400/5 p-8">
-					<dt class="text-sm/6 font-semibold text-gray-600">Flat platform fee</dt>
-					<dd class="order-first text-3xl font-semibold tracking-tight text-gray-900">3%</dd>
-				</div>
-				<div class="flex flex-col bg-gray-400/5 p-8">
-					<dt class="text-sm/6 font-semibold text-gray-600">Uptime guarantee</dt>
-					<dd class="order-first text-3xl font-semibold tracking-tight text-gray-900">99.9%</dd>
-				</div>
-				<div class="flex flex-col bg-gray-400/5 p-8">
-					<dt class="text-sm/6 font-semibold text-gray-600">Paid out to creators</dt>
+					<dt class="text-sm/6 font-semibold text-gray-600">На смесители</dt>
 					<dd class="order-first text-3xl font-semibold tracking-tight text-gray-900">$70M</dd>
 				</div>
 			</dl>
