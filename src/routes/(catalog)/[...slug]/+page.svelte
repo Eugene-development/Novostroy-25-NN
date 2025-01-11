69586b4df25fd3ef162fe0edadbf21d05fb8d776
Eugene-development <52544906+Eugene-development@users.noreply.github.com>
2025-01-11 18:33:44 +0300

    с

--- src/routes/(catalog)/[...slug]/+page.svelte
+++ src/routes/(catalog)/[...slug]/+page.svelte
@@ -4,7 +4,25 @@
 	import { Catalog, Rubric, Category, Product } from '../UI';
 
 	let { data } = $props();
+
+	$inspect(data);
 </script>
+
+<svelte:head>
+	{#if data.isCatalog}
+		<title>{data.req.catalog.metaTitle.value}</title>
+		<meta name="description" content={data.req.catalog.metaDescription.value} />
+	{:else if data.isRubric}
+		<title>{data.req.rubric.metaTitle.value}</title>
+		<meta name="description" content={data.req.rubric.metaDescription.value} />
+	{:else if data.isCategory}
+		<title>{data.req.category.metaTitle.value}</title>
+		<meta name="description" content={data.req.category.metaDescription.value} />
+	{:else if data.isProduct}
+		<title>Новострой | {data.req.product.value}</title>
+		<meta name="description" content="{data.req.product.value} подбирается из категории {data.req.product.parentable.value} с учётом ваших пожеланий, чтобы создать уют и комфорт в соответствии с вашим дизайн-проектом."/>
+	{/if}
+</svelte:head>
 
 <main class="h-full flex-1 overflow-y-auto py-3 lg:pl-4">
 	{#if data.isCatalog}
