d2328d737c023cedfd4effec895fcd098ce55513
Eugene-development <52544906+Eugene-development@users.noreply.github.com>
2025-01-29 00:50:39 +0300

    c

--- src/routes/layout/header/UI/Menu/MobileMenu/index.svelte
+++ src/routes/layout/header/UI/Menu/MobileMenu/index.svelte
@@ -226,7 +226,7 @@
 						Москва и МО
 					</p> -->
 						<a
-							href="/acctions"
+							href="/actions"
 							class="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-gray-900 hover:bg-gray-50"
 							>Акции</a
 						>
