2f23ffa8e1ee749419f475a0efe2c98ef18aa19f
Eugene-development <52544906+Eugene-development@users.noreply.github.com>
2025-01-29 11:19:43 +0300

    c

--- src/routes/layout/header/UI/Menu/MobileMenu/CatalogItem/index.svelte
+++ src/routes/layout/header/UI/Menu/MobileMenu/CatalogItem/index.svelte
@@ -1,6 +1,8 @@
 <script>
 	import { DropdownMenu } from 'bits-ui';
 
+    import RubricItem from './RubricItem/index.svelte';
+
 	import { showRubricMenu } from '$lib/state/showRubricMenu.svelte';
 
 	let { catalog } = $props();
@@ -31,29 +33,7 @@
 {#if showRubricMenu.value}
 	<div class="flex flex-col">
 		{#each catalog.rubric as rubric}
-			<DropdownMenu.Root>
-				<DropdownMenu.Trigger>
-					<span
-						class="ml-10 flex w-full rounded-lg px-1 py-2 pr-3.5 text-base/7 font-semibold text-red-700 hover:bg-gray-50"
-					>
-						{rubric.value}
-					</span>
-				</DropdownMenu.Trigger>
-
-				<DropdownMenu.Content
-					class="w-full max-w-[229px] rounded-xl border border-muted bg-gray-50 py-1.5  shadow-xl outline-none"
-				>
-					{#each rubric.category as category}
-						<a href="/" class="hover:bg-gray-200">
-							<DropdownMenu.Item
-								class="rounded-button flex h-10 select-none items-center px-6 py-3 text-sm font-medium outline-none data-[highlighted]:bg-muted"
-							>
-								{category.value}
-							</DropdownMenu.Item>
-						</a>
-					{/each}
-				</DropdownMenu.Content>
-			</DropdownMenu.Root>
+			<RubricItem {rubric} />
 		{/each}
 	</div>
 {/if}
