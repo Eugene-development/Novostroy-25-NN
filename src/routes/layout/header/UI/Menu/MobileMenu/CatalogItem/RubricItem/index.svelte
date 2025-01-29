7dbf74b7455eea046b0e48ab7a44d2dd8ef9ccaf
Eugene-development <52544906+Eugene-development@users.noreply.github.com>
2025-01-29 11:48:12 +0300

    c

--- src/routes/layout/header/UI/Menu/MobileMenu/CatalogItem/RubricItem/index.svelte
+++ src/routes/layout/header/UI/Menu/MobileMenu/CatalogItem/RubricItem/index.svelte
@@ -9,19 +9,19 @@
 <DropdownMenu.Root>
     <DropdownMenu.Trigger>
         <span
-            class="ml-10 flex w-full rounded-lg px-1 py-2 pr-3.5 text-base/7 font-semibold text-red-700 hover:bg-gray-50"
+            class="ml-10 flex w-full rounded-lg p-2 my-1 text-base/7 font-semibold text-gray-900 bg-gray-50"
         >
             {rubric.value}
         </span>
     </DropdownMenu.Trigger>
 
     <DropdownMenu.Content
-        class="w-full max-w-[229px] rounded-xl border border-muted bg-gray-50 py-1.5  shadow-xl outline-none"
+        class="w-full max-w-[229px] rounded-xl border-gray-200 border-2 bg-gray-50 py-1.5  shadow-xl outline-none"
     >
         {#each rubric.category as category}
-            <a href="/" class="hover:bg-gray-200">
+            <a href="/" class="">
                 <DropdownMenu.Item
-                    class="rounded-button flex h-10 select-none items-center px-6 py-3 text-sm font-medium outline-none data-[highlighted]:bg-muted"
+                    class="rounded-button  flex h-10 select-none items-center px-6 py-3 text-sm font-medium outline-none data-[highlighted]:bg-muted"
                 >
                     {category.value}
                 </DropdownMenu.Item>
