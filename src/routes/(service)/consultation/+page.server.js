3ed0d60489ccfd11f358aadb086ac766f6558d72
Eugene-development <52544906+Eugene-development@users.noreply.github.com>
2025-02-02 19:47:28 +0300

    c

---
+++ src/routes/(service)/consultation/+page.server.js
@@ -0,0 +1,29 @@
+/** @type {import('./$types').PageServerLoad} */
+import axios from 'axios';
+
+export const actions = {
+	sendFormMeasurement: async ({ request }) => {
+		try {
+			const data = await request.formData();
+			const url = `/send-form-measurement`;
+			const urlMAIL = {
+				baseURL: import.meta.env.VITE_MAIL,
+				headers: {
+					Authorization: `Bearer ${import.meta.env.VITE_KEY}`
+				}
+			};
+			const variables = {
+				name: data.get('client-name'),
+				// phone: data.get('client-phone'),
+				// email: data.get('client-email'),
+				// address: data.get('client-address'),
+				// comment: data.get('client-comment')
+			};
+			await axios.post(url, variables, urlMAIL);
+			return { success: true };
+		} catch (error) {
+			console.error('Error sending form:', error);
+			return { success: false, error: error.message };
+		}
+	}
+};
