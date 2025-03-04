9dda2d12637c039148336e775d1148d853d09b8f
Eugene-development <52544906+Eugene-development@users.noreply.github.com>
2025-03-04 21:49:16 +0300

    с

--- src/routes/(catalog)/UI/Category/Carousel/index.svelte
+++ src/routes/(catalog)/UI/Category/Carousel/index.svelte
@@ -11,7 +11,7 @@
 				<img
 					src={`${import.meta.env.VITE_S3}/catalog/${item.hash}`}
 					alt="Выполненный проект"
-					class="flex aspect-[4/3] items-center justify-center"
+					class="flex  items-center justify-center object-contain"
 				/>
 			</Carousel.Item>
 		{/each}
@@ -20,3 +20,43 @@
 	<Carousel.Previous class="ml-8" />
 	<Carousel.Next class="mr-8" />
 </Carousel.Root>
+
+
+
+
+<!-- <script>
+    import * as Carousel from '$lib/components/ui/carousel/index.js';
+    import { onMount } from 'svelte';
+
+    let { data } = $props();
+
+    // Функция для определения аспект-соотношения
+    function setAspectRatio(event) {
+        const img = event.target;
+        if (img.naturalWidth > img.naturalHeight) {
+            img.classList.add('aspect-[4/3]');
+            img.classList.remove('aspect-[3/4]');
+        } else {
+            img.classList.add('aspect-[3/4]');
+            img.classList.remove('aspect-[4/3]');
+        }
+    }
+</script>
+
+<Carousel.Root class="w-full">
+    <Carousel.Content>
+        {#each data as item, i (i)}
+            <Carousel.Item>
+                <img
+                    src={`${import.meta.env.VITE_S3}/catalog/${item.hash}`}
+                    alt="Выполненный проект"
+                    class="flex items-center justify-center"
+                    onload={setAspectRatio}
+                />
+            </Carousel.Item>
+        {/each}
+    </Carousel.Content>
+
+    <Carousel.Previous class="ml-8" />
+    <Carousel.Next class="mr-8" />
+</Carousel.Root> -->
