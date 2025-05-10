9b47682aba967f36a70f2893fd6807c498ff9b04
Eugene-development <52544906+Eugene-development@users.noreply.github.com>
2025-05-10 14:32:50 +0300

    c

--- src/routes/layout/modals/CheckCity/index.svelte
+++ src/routes/layout/modals/CheckCity/index.svelte
@@ -71,7 +71,7 @@
 
         </div>
         <div class="mt-5 sm:mt-6">
-          <button type="button" class="inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Подтвердить</button>
+          <button type="button" onclick={() => (visibleCheckCity.value = false)} class="inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Подтвердить</button>
         </div>
       </div>
     </div>
